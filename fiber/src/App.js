44428338e5a09fdea7f529dd4ca90c885806357c
Ben Ilegbodu <ben@benmvp.com>
2017-08-05 23:58:59 -0700

    Create two separate apps for v15 & v16
    Going to need to test some examples in both old React (v15) and new React (v16) so just creating two apps

---
+++ fiber/src/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+import "./App.css";
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <h1>Fiber Reconciler</h1>
+      </div>
+    );
+  }
+}
